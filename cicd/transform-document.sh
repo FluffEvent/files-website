c634d004635548a8ffb1ebedc546a505c8355e19
Matiboux <matiboux@gmail.com>
2025-03-09 00:57:22 +0100

    Update transform document script

--- cicd/transform-document.sh
+++ cicd/transform-document.sh
@@ -15,8 +15,11 @@ fi
 
 echo "Transforming file '$FILE'..."
 
-# Replace '.....' with long input line
-sed -i 's|\.\{5\}|<span class="input-line input-line-long"></span>|g' "$FILE"
+# Replace '\[.....\]' or '.....' with long input line
+sed -i 's/\\\[\.\{5\}\\\]\|\.\{5\}/<span class="input-line input-line-long"><\/span>/g' "$FILE"
 
-# Replace '....' with standard input line
-sed -i 's|\.\{4\}|<span class="input-line"></span>|g' "$FILE"
+# Replace '\[....\]' with two standard input lines
+sed -i 's/\\\[\.\{4\}\\\]/<span class="input-line"><\/span><span class="input-line input-line-next"><\/span><span class="input-line input-line-next"><\/span><span class="input-line input-line-next"><\/span>/g' "$FILE"
+
+# Replace '\[...\]' or '....' with standard input line
+sed -i 's/\\\[\.\{3\}\\\]\|\.\{4\}/<span class="input-line"><\/span><span class="input-line input-line-next"><\/span>/g' "$FILE"
