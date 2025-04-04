8ba28c946478eac8bcccbc049a54b5cce201806f
Matiboux <matiboux@gmail.com>
2025-04-05 00:31:20 +0200

    Add content config

---
+++ app/app/src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
+
+export const collections = {
+	documents: defineCollection({
+		loader: glob({ pattern: '**/*.md', base: './src/content/documents' }),
+		schema: z.object({
+			version: z.string().optional(),
+		}),
+	}),
+	'private-documents': defineCollection({
+		loader: glob({ pattern: '**/*.md', base: './src/content/private-documents' }),
+		schema: z.object({
+			version: z.string().optional(),
+		}),
+	}),
+}
