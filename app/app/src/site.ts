b78c5df8e31ae1995ab805bc43e930060067d990
Mati <matiboux@gmail.com>
2025-10-02 02:50:42 +0200

    Update site template (#13)

--- app/app/src/site.ts
+++ app/app/src/site.ts
@@ -1,3 +1,5 @@
+import { GITHUB_SHA, VERSION_TAG } from 'astro:env/client'
+
 import type { Props as BaseProps } from '~/layouts/Base.astro'
 
 export interface Site
@@ -5,6 +7,7 @@ export interface Site
 	lang?: BaseProps['lang']
 	title?: BaseProps['title']
 	description?: BaseProps['description']
+	version?: BaseProps['version']
 	author?: BaseProps['author']
 	keywords?: BaseProps['keywords']
 	generator?: BaseProps['generator']
@@ -26,6 +29,7 @@ export const site: Site = {
 		'en': 'Fluff Event association documents render in printable web pages.',
 		'fr': 'Documents de l\'association Fluff Event rendus en pages web imprimables.',
 	},
+	version: GITHUB_SHA || VERSION_TAG || 'dev',
 	author: 'Matiboux',
 	themeColor: '#ffffff',
 	viewportScale: 1,
