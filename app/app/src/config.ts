7c9089b86fca31d89a37d5c29918432af3c4cd7a
Matiboux <matiboux@gmail.com>
2025-10-02 02:37:41 +0200

    Update i18n config

--- app/app/src/config.ts
+++ app/app/src/config.ts
@@ -1,26 +1,45 @@
 import type { AstroConfig } from 'astro'
 
-type LocaleKeys = Record<string, Record<string, string>>
-type I18nConfig = AstroConfig['i18n'] & { localeKeys?: LocaleKeys }
+import en from './i18n/locales/en'
+import fr from './i18n/locales/fr'
+import de from './i18n/locales/de'
+import es from './i18n/locales/es'
 
-export const i18n: I18nConfig = {
-	locales: [
-		{
-			codes: ['en', 'en_US'],
-			path: 'en',
-		},
-		{
-			codes: ['fr', 'fr_FR'],
-			path: 'fr',
-		},
-	],
-	defaultLocale: 'fr',
+export interface LocaleKeys
+{
+	[key: string]: LocaleKeys | string
+}
+
+export type LocalesKeys = Record<string, LocaleKeys>
+
+export type I18nConfig = AstroConfig['i18n'] & { localeKeys?: LocalesKeys }
+
+export const i18nLocales = [
+	{
+		codes: ['fr', 'fr_FR'],
+		path: 'fr',
+	},
+	{
+		codes: ['en', 'en_US'],
+		path: 'en',
+	},
+] as const satisfies I18nConfig['locales']
+
+export const i18nDefaultLocale = i18nLocales[0].path
+
+export const i18n = {
+	locales: i18nLocales,
+	defaultLocale: i18nDefaultLocale,
 	fallback: {
 		en: 'fr',
+	},
+	localeKeys: {
+		en,
+		fr,
 	},
 	routing: {
 		prefixDefaultLocale: false,
 		redirectToDefaultLocale: true,
 		fallbackType: 'rewrite',
 	},
-}
+} as const satisfies I18nConfig
