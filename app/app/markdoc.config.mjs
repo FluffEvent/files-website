0da80374aece81a3873818c086569960fe8964c7
Matiboux <matiboux@gmail.com>
2025-10-15 23:44:02 +0200

    Update I18n markdoc component

--- app/app/markdoc.config.mjs
+++ app/app/markdoc.config.mjs
@@ -3,10 +3,9 @@ import { defineMarkdocConfig, component } from '@astrojs/markdoc/config'
 export default defineMarkdocConfig({
 	tags: {
 		i18n: {
-			render: component('./src/components/markdoc/Translate.astro'),
+			render: component('./src/components/markdoc/I18n.astro'),
 			attributes: {
-				en: { type: String, required: true },
-				fr: { type: String },
+				locales: { type: Object, required: true },
 			},
 		},
 	},
