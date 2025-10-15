e5eda01d58dfaa0cadaf7651a4055189cef9d366
Matiboux <matiboux@gmail.com>
2025-10-15 22:00:47 +0200

    Refactor shell vars

--- cicd/version-document.sh
+++ cicd/version-document.sh
@@ -7,8 +7,8 @@ if [ $# -gt 0 ]; then
 	FILE="$1"
 	shift
 fi
-if [ ! -f "$FILE" ]; then
-	echo "File '$FILE' does not exist"
+if [ ! -f "${FILE}" ]; then
+	echo "File '${FILE}' does not exist"
 	exit 1
 fi
 
@@ -18,25 +18,25 @@ if [ $# -gt 0 ]; then
 	VERSION="$1"
 	shift
 fi
-if [ -z "$VERSION" ]; then
+if [ -z "${VERSION}" ]; then
 	echo "Version is required"
 	exit 1
 fi
 
-echo "Adding version '$VERSION' to file '$FILE'..."
+echo "Adding version '${VERSION}' to file '${FILE}'..."
 
 # Check for existing front matter
-if [ "$(head -n 1 "$FILE")" = '---' ]; then
+if [ "$(head -n 1 "${FILE}")" = '---' ]; then
 
 	# Get line number of closing front matter delimiter
-	LINE=$(grep -m 2 -n '^---$' "$FILE" | tail -n 1 | cut -d ':' -f 1)
+	LINE=$(grep -m 2 -n '^---$' "${FILE}" | tail -n 1 | cut -d ':' -f 1)
 
 	# Append version to front matter
-	sed -i "5i\version: '$VERSION'" "$FILE"
+	sed -i "5i\version: '${VERSION}'" "${FILE}"
 
 else
 
 	# Prepend new front matter with version
-	sed -i "1i\---\nversion: '$VERSION'\n---\n" "$FILE"
+	sed -i "1i\---\nversion: '${VERSION}'\n---\n" "${FILE}"
 
 fi
