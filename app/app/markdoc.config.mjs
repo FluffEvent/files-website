bf243d4cda5a7a1d274ff985ebc6b328c4d4c6c3
Matiboux <matiboux@gmail.com>
2025-11-01 17:38:20 +0100

    Remove markdoc i18n component

--- app/app/markdoc.config.mjs
+++ app/app/markdoc.config.mjs
@@ -1,13 +1,3 @@
 import { defineMarkdocConfig, component } from '@astrojs/markdoc/config'
 
-export default defineMarkdocConfig({
-	tags: {
-		i18n: {
-			render: component('./src/components/markdoc/I18n.astro'),
-			attributes: {
-				locales: { type: Object, required: true },
-				renderMarkdown: { type: Boolean, default: false },
-			},
-		},
-	},
-})
+export default defineMarkdocConfig({})
